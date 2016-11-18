9c4aac9888bce3dbd41fed9e87547ff1d86decb0
Anwar-Nasraden <anesredin638@gmail.com>
2016-11-18 16:07:56 +0000

    products added

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,6 +1,19 @@
 package controllers;
+
+import play.api.Environment;
 import play.mvc.*;
+import play.data.*;
+import play.db.ebean.Transactional;
+
+import java.util.*;
+import javax.inject.Inject;
+
 import views.html.*;
+
+//import models
+import models.*;
+
+
 
 public class HomeController extends Controller {
 
@@ -13,7 +26,11 @@ public class HomeController extends Controller {
     }
 
     public Result products() {
-        return ok(products.render());
+        //Get the list of products
+        List<Product> productslist = Product.findAll();
+
+        // Render the list products 
+        return ok(products.render(productslist));
     }
 
 }
